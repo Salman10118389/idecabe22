b7d10607312d734ad11f75c1e28f003ec60f8b6e
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2023-02-18 12:31:40 +0700

    commit upload camera

--- app/src/main/java/com/example/idecabe2/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/idecabe2/ui/camera/CameraFragment.kt
@@ -1,21 +1,48 @@
 package com.example.idecabe2.ui.camera
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.impl.utils.ContextUtil
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.idecabe2.R
+import com.example.idecabe2.data.model.Project
 import com.example.idecabe2.databinding.FragmentCameraBinding
 import com.example.idecabe2.databinding.FragmentHomeBinding
 import com.example.idecabe2.ui.auth.AuthViewModel
 import com.example.idecabe2.utils.UiState
 import com.example.idecabe2.utils.hide
 import com.example.idecabe2.utils.show
+import com.example.idecabe2.utils.toast
+import com.google.firebase.firestore.ServerTimestamp
+import com.google.firebase.storage.StorageReference
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
 
 @AndroidEntryPoint
 class CameraFragment : Fragment() {
@@ -23,6 +50,11 @@ class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
     private val cameraViewModel: CameraViewModel by viewModels()
     private val authViewModel: AuthViewModel by viewModels()
+    private var imageUris:MutableList<Uri> = arrayListOf()
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraExecutorService: ExecutorService
+    private lateinit var objProject: Project
 
 
     //connect to adapter
@@ -40,9 +72,130 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        startCamera()
+
     }
     override fun onDestroyView() {
         super.onDestroyView()
     }
+
+    fun takePhoto(){
+        // Get a stable reference of modifiable image Capture Use Case
+        var imagaCapture: ImageCapture = imageCapture ?: return
+
+//      Create Timestamp Name & MediaStore Directory
+        val imageName = SimpleDateFormat(FILE_NAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, imageName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (SDK_INT > Build.VERSION_CODES.M){
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/IdeCabe-Image")
+            }
+        }
+
+//      Create options object contains file + metadata
+        val outputOptions = activity?.contentResolver?.let {
+            ImageCapture.OutputFileOptions
+                .Builder(
+                    it,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    contentValues
+                ).build()
+        }
+        if (outputOptions != null) {
+            imageCapture!!.takePicture(
+                outputOptions,
+                activity?.let { ContextCompat.getMainExecutor(it.applicationContext) }!!,
+                object: ImageCapture.OnImageSavedCallback{
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        val msg ="Photo capture succeeded: ${outputFileResults.savedUri}"
+                        val uri = outputFileResults.savedUri
+                        val name = uri.toString()
+//                        Send data to firebase
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        Log.e(TAG, "onError: ${exception.message}", exception)
+                    }
+                }
+            )
+        }
+    }
+
+    companion object {
+        private const val FILE_NAME_FORMAT = "yyyy-MM-dd-SSS"
+        private const val TAG = "Camera Fragment"
+    }
+
+//    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all{
+//        activity?.let { it1 -> ContextCompat.checkSelfPermission(it1.baseContext, it) } == PackageManager.PERMISSION_GRANTED
+//    }
+
+//    companion object {
+//
+//        val PERMISSIONS = arrayOf(
+//            Manifest.permission.CAMERA,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//            Manifest.permission.READ_EXTERNAL_STORAGE
+//        )
+//        private const val FILE_NAME_FORMAT = "yyyy-MM-dd-SSS"
+////        private const val REQUEST_PERMISSION_CODE = 10
+////
+//////        Required Permission
+////        private val REQUIRED_PERMISSIONS =
+////            mutableListOf(
+////                android.Manifest.permission.CAMERA
+////            ).apply {
+////                if (SDK_INT <= Build.VERSION_CODES.P){
+////                    add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+////                }
+////            }.toTypedArray()
+//    }
+
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        if (requestCode == REQUEST_PERMISSION_CODE){
+//            if (allPermissionGranted())
+//                startCamera()
+//        }else {
+//            toast("Permission not granted by the user")
+//        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//    }
+
+
+    
+
+    private fun startCamera() {
+        val cameraProvideFuture = activity?.let { ProcessCameraProvider.getInstance(it.applicationContext) }
+
+        cameraProvideFuture?.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProvideFuture.get()
+            //camera preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider { binding.cameraPrev.surfaceProvider }
+                }
+
+            //Take Picture
+            imageCapture =ImageCapture.Builder().build()
+            //Camera Selector
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this@CameraFragment, cameraSelector,preview , imageCapture)
+            }catch (e: Exception){
+                Log.e(TAG, "startCamera: ", e)
+            }
+        }, activity?.let { ContextCompat.getMainExecutor(it.applicationContext) })
+        objProject = arguments?.getParcelable("project")!!
+        Log.d(TAG, "startCamera: $objProject")
+    }
+
 
 }
--- app/src/main/java/com/example/idecabe2/ui/projectDetail/ProjectDetailFragment.kt
+++ app/src/main/java/com/example/idecabe2/ui/projectDetail/ProjectDetailFragment.kt
@@ -1,7 +1,12 @@
 package com.example.idecabe2.ui.projectDetail
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,6 +15,7 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -17,6 +23,7 @@ import com.example.idecabe2.R
 import com.example.idecabe2.data.model.Project
 import com.example.idecabe2.databinding.FragmentHomeBinding
 import com.example.idecabe2.databinding.FragmentProjectDetailBinding
+import com.example.idecabe2.ui.camera.CameraActivity
 import com.example.idecabe2.ui.home.HomeAdapter
 import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +32,7 @@ private const val TAG = "ProjectDetailFragment"
 @AndroidEntryPoint
 class ProjectDetailFragment : Fragment() {
 
-    private lateinit var imageUri: MutableList<Uri> = arrayListOf()
+    private var imageUri: MutableList<Uri> = arrayListOf()
     private lateinit var binding: FragmentProjectDetailBinding
     private lateinit var objProject: Project
 
@@ -40,44 +47,103 @@ class ProjectDetailFragment : Fragment() {
         return binding.root
     }
 
-    private val startForProfileImageResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-            val resultCode = result.resultCode
-            val data = result.data
-
-            if (resultCode == Activity.RESULT_OK) {
-                //Image Uri will not be null for RESULT_OK
-                val fileUri = data?.data!!
-
-                imageUri.add(fileUri)
-                imgProfile.setImageURI(fileUri)
-            } else if (resultCode == ImagePicker.RESULT_ERROR) {
-                Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
-            }
-        }
+//    private val startForProfileImageResult =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+//            val resultCode = result.resultCode
+//            val data = result.data
+//
+//            if (resultCode == Activity.RESULT_OK) {
+//                //Image Uri will not be null for RESULT_OK
+//                val fileUri = data?.data!!
+//
+//                imageUri.add(fileUri)
+//                binding.image.setImageURI(fileUri)
+//            } else if (resultCode == ImagePicker.RESULT_ERROR) {
+//                Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getProjectBundle()
-
         binding.btnUploadImages.setOnClickListener {
-            ImagePicker.with(this)
-                .crop()
-                .compress(1024)
-                .maxResultSize(1080, 1080)
-                .start()
+            camera()
         }
     }
     fun getProjectBundle(){
         arguments.let {
-            objProject = it?.getParcelable<Project>("project")!!
+            objProject = it?.getParcelable("project")!!
             Log.d(TAG, "getProjectBundle: $it")
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+    }
+
+    //Move to Fragment Camera
+
+    private fun hasPermissions(context: Context, licenses: Array<String>): Boolean = licenses.all {
+        ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val permReqLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { licenses ->
+            val granted = licenses.entries.all {
+                it.value == true
+            }
+            if (granted) {
+
+            }
+        }
+
+    companion object {
+        val PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+//        private const val REQUEST_PERMISSION_CODE = 10
+//
+////        Required Permission
+//        private val REQUIRED_PERMISSIONS =
+//            mutableListOf(
+//                android.Manifest.permission.CAMERA
+//            ).apply {
+//                if (SDK_INT <= Build.VERSION_CODES.P){
+//                    add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                }
+//            }.toTypedArray()
+    }
+
+    private fun camera(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            displayCameraFragment()
+            return
+        }else {
+            activity?.let {
+                if (hasPermissions(activity as Context, PERMISSIONS)){
+                    displayCameraFragment()
+                }else {
+                    permReqLauncher.launch(
+                        PERMISSIONS
+                    )
+                }
+            }
+        }
+    }
+
+    private fun displayCameraFragment(){
+        val bundle = Bundle()
+        bundle.putString("project_id", objProject.id)
+        val bundle_userId = Bundle()
+        bundle_userId.putString("user_id", objProject.user_id)
+        Log.d(TAG, "onViewCreated: ${objProject.id}")
+        val intentCamera = Intent(activity?.applicationContext, CameraActivity::class.java)
+        intentCamera.putExtra("bundle_project_image", bundle)
+        intentCamera.putExtra("bundle_userId", bundle_userId)
+        startActivity(intentCamera)
     }
 }
